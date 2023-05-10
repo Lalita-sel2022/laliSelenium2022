5ea83793833997d6be60d9c432e37af54a09e381
Lalita-sel2022 <lalitasaini12918@gmail.com>
2023-05-10 10:09:43 +0530

    Commit to laliSelenium2022

---
+++ TestNG-2023/src/test/java/excelReaderOperation/DataDemo.java
@@ -0,0 +1,17 @@
+package excelReaderOperation;
+
+
+public class DataDemo {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Row number is :");
+		//ExcelAPI ex= new ExcelAPI("C:\\Users\\DELL\\Desktop\\TestCases.xlsx");
+		System.out.println("Row number is :");
+		//int no = ex.getRowCount("login");
+		//System.out.println("Row number is :"+no);
+
+	}
+
+}
